f8843135489931cfe59363852650c3dfbff729db
katemskl <moskal4uk.k@gmail.com>
2022-05-17 17:58:27 +0300

    added ship speed adjustment

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,7 +14,7 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
     # Создание корабля
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # Запуск нового экрана игры
     while True:
--- ship.py
+++ ship.py
@@ -2,9 +2,10 @@ import pygame
 
 
 class Ship:
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Инициализирует корабль и задает его начальную позицию"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Загрузка изображения корабля и получние прямоугольника
         self.image = pygame.image.load('images/ship2.bmp')
@@ -15,16 +16,22 @@ class Ship:
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
-        # Флаш перемещения
+        # Сохранение вещественной координаты центра корабля
+        self.center = float(self.rect.centerx)
+
+        # Флаги перемещения
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
-        """Обновляем озицию корабля с учетом флага."""
+        """Обновляем озицию корабля с учетом флагов."""
+        # Обновляем атрибут center, не rect
         if self.moving_right:
-            self.rect.centerx += 1
+            self.rect.centerx += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.rect.centerx -= self.ai_settings.ship_speed_factor
+        # Обновление атрибута rect на основании self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Рисует корабль в текущей позиции"""
