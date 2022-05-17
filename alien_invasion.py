1489d76167b90f31cf02176d38d8990f146d1af3
katemskl <moskal4uk.k@gmail.com>
2022-05-17 13:38:45 +0300

    added new functional: ship can move right

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,7 +18,8 @@ def run_game():
 
     # Запуск нового экрана игры
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 
--- ship.py
+++ ship.py
@@ -15,6 +15,14 @@ class Ship:
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # Флаш перемещения
+        self.moving_right = False
+
+    def update(self):
+        """Обновляем озицию корабля с учетом флага."""
+        if self.moving_right:
+            self.rect.centerx += 1
+
     def blitme(self):
         """Рисует корабль в текущей позиции"""
         self.screen.blit(self.image, self.rect)
