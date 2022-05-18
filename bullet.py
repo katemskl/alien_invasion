89110eb8a9f4d598b6853225b64cbe066e2cc4e4
katemskl <moskal4uk.k@gmail.com>
2022-05-18 17:46:51 +0300

    added new functional for bullet

---
+++ bullet.py
@@ -0,0 +1,37 @@
+import pygame
+
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    """Класс для управления пулями, выпущенных кораблем"""
+
+    def __init__(self, ai_settings, screen, ship):
+        """Создает обьект пули в текущей позиции корабля"""
+
+        super().__init__()
+        self.screen = screen
+
+        # Создание пули в позиции (0, 0) и назначение правильнойй позиции
+        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
+        self.rect.centerx = ship.rect.centrx
+        self.rect.top = ship.rect.top
+
+        # Позиция пули храниться в вещественном формате
+        self.y = float(self.rect.y)
+
+        self.color = ai_settings.bullet_color
+        self.speed_factor = ai_settings.bullet_speed_factor
+
+    def update(self):
+        """Перемещает пулю вверх по экрану"""
+        # Обновление позиции пули в вещественном формате
+        self.y -= self.speed_factor
+        # Обновление позиции треугольника
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Вывод пули на экран"""
+
+        pygame.draw.rect(self.screen, self.color, self.rect)
+
+
