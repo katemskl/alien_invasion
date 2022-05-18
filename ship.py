574be3ea7bdd9daac7125a18c50f623f62e27b67
katemskl <moskal4uk.k@gmail.com>
2022-05-18 06:29:37 +0300

    added movement restriction

--- ship.py
+++ ship.py
@@ -26,9 +26,9 @@ class Ship:
     def update(self):
         """Обновляем озицию корабля с учетом флагов."""
         # Обновляем атрибут center, не rect
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.rect.centerx += self.ai_settings.ship_speed_factor
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.rect.centerx -= self.ai_settings.ship_speed_factor
         # Обновление атрибута rect на основании self.center
         self.rect.centerx = self.center
