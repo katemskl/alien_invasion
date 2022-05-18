761fcbc9b1afebb2b53638fa368f14520252bc3b
katemskl <moskal4uk.k@gmail.com>
2022-05-18 18:18:06 +0300

    added limited amount of active bullets and improved general code

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,12 +24,7 @@ def run_game():
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        bullets.update()
-        # Удаление пуль, вышедших за край экрана
-        for bullet in bullets.copy():
-            if bullet.rect.bottom < 0:
-                bullets.remove(bullet)
-        # print(len(bullets))
+        gf.update_bullets(bullets)
         gf.update_screen(ai_settings, screen, ship, bullets)
 
 
--- game_functions.py
+++ game_functions.py
@@ -5,6 +5,17 @@ import pygame
 from bullet import Bullet
 
 
+def update_bullets(bullets):
+    """Обновляет позиции пуль и уничтожает старые пули"""
+    # Обновляет позиции пуль
+    bullets.update()
+    # Удаление пуль, вышедших за край экрана
+    for bullet in bullets.copy():
+        if bullet.rect.bottom < 0:
+            bullets.remove(bullet)
+    # print(len(bullets))
+
+
 def check_keydown_events(event,ai_settings, screen, ship, bullets):
     """ Реагирует на нажатие клавиш"""
     if event.key == pygame.K_RIGHT:
@@ -13,8 +24,9 @@ def check_keydown_events(event,ai_settings, screen, ship, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         # Создание новой пули и включение ее в групу bullets
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
+        if len(bullets) < ai_settings.bullets_allowed:
+            new_bullet = Bullet(ai_settings, screen, ship)
+            bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
