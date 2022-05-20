0b9a1aabbee654ee6a0c30c4deecef9dcda2976a
katemskl <moskal4uk.k@gmail.com>
2022-05-20 07:26:58 +0300

    refactoring of functions

--- game_functions.py
+++ game_functions.py
@@ -16,17 +16,22 @@ def update_bullets(bullets):
     # print(len(bullets))
 
 
-def check_keydown_events(event,ai_settings, screen, ship, bullets):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """ Реагирует на нажатие клавиш"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # Создание новой пули и включение ее в групу bullets
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Выпускает пулю, если максмум еще не достигнут"""
+    # Создание новой пули и включение ее в групу bullets
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
@@ -35,6 +40,8 @@ def check_keyup_events(event, ship):
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
+    elif event.key == pygame.K_q:
+        sys.exit()
 
 
 def check_events(ai_settings, screen, ship, bullets):
