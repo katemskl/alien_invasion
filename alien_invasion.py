cb5dd37943d1f6f1f4671e525a3f796338afa527
katemskl <moskal4uk.k@gmail.com>
2022-05-20 09:05:12 +0300

    added number_rows

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,7 +21,7 @@ def run_game():
     aliens = Group()
 
     # Создание флота пришельцев
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # Запуск нового экрана игры
     while True:
