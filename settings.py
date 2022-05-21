4e6cebba5a58d9e7c2817f57bfce1390bb895867
katemskl <moskal4uk.k@gmail.com>
2022-05-21 16:35:56 +0300

    improved moving of aliens fleet

--- settings.py
+++ settings.py
@@ -20,3 +20,6 @@ class Settings:
 
         # Настройки пришельцев
         self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1 обозначает движение в право, а -1 - влево
+        self.fleet_direction = 1
